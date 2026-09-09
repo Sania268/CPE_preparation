61cdc0b7f8d32afe0b503468e73d9327c5f6d3b8
SANIA SAEL <159134444+Sania268@users.noreply.github.com>
2026-09-09 20:08:25 +0800

    11639. Guard the land.cpp

--- 11639. Guard the land.cpp
+++ 11639. Guard the land.cpp
@@ -3,6 +3,19 @@
 #include<cassert>
 
 void (int v) {
-  
+  assert(v >= 0);
+  assert (v <= 100);
 }
 
+int main () {
+
+  int T;
+  cin >> T;
+  int cas = 1;
+  assert(T <= 100);
+  while (T -- ) {
+    
+  }
+  
+  return 0;
+}
