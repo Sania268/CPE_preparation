5435769bcf0323c30f0b7436e5f753e4bba5078f
SANIA SAEL <159134444+Sania268@users.noreply.github.com>
2026-09-17 10:57:11 +0800

    Implement happy number detection
    Added logic to determine if a number is happy or unhappy.

--- 10591.Happy number.cpp
+++ 10591.Happy number.cpp
@@ -6,7 +6,36 @@ int main () {
   int T, n, cas = 1;
   cin >> T;
 
-  
+  while (T--) {
+    set<int>all;
+    cin >> n;
+    int original = n;
+
+    while (n != 1) {
+      int sum = 0;
+      
+     if (all.count(n))
+      break;
+      
+      all.insert(n);
+
+      while (n > 0) {
+        int digit = n % 10;
+        sum += digit * digit;
+        n/= 10;
+      }
+      n = sum;
+    }
+
+    if (n == 1) {
+      cout << "Cas #" << cas << ": " << original << " is a Happy number." << endl;
+    }
+    else {
+      cout << "cas #" << cas << ": " << original << " is an Unhappy number."<< endl;
+    }
+    cas++;
+    
+  }
   return 0;
 }
 
