3fd81fd207f5759a9e8895bf400f3b9ced6621f5
SANIA SAEL <159134444+Sania268@users.noreply.github.com>
2026-09-19 15:04:40 +0800

    Add comments to clarify algorithm steps
    Added comments to outline the algorithm steps.

--- 10010. Where 's Waldorf ?.cpp
+++ 10010. Where 's Waldorf ?.cpp
@@ -1,1 +1,9 @@
+// Find a possible W
+// Read the grid
+// Read a word
+// Try each of the 8 directions
+// Keep moving in this same direction
+// Check whether the following letters match
+// If yes, found the starting position
+
 
