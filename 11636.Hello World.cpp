802796cb5b7b206dca8d4a263dd7bee5be5aa674
SANIA SAEL <159134444+Sania268@users.noreply.github.com>
2026-09-24 09:43:38 +0800

    Fix case increment logic in Hello World program

--- 11636.Hello World.cpp
+++ 11636.Hello World.cpp
@@ -17,10 +17,10 @@ int main () {
     for (int i = 0; i < 20; i++) {
       if (a[i] >= n) {
         cout << "Case " << cas << ": "<< i << endl;
-        cas++;
         break;
       }
     }
+    cas++;
   }
   
   
