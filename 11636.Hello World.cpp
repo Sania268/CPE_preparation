9116c54e17e45f89be959a1ae7787d44e0744b3b
SANIA SAEL <159134444+Sania268@users.noreply.github.com>
2026-09-24 09:34:28 +0800

    Implement array of powers of two and input handling
    Added an array to store powers of two and implemented a loop to find the first index where the value is greater than or equal to input 'n'.

--- 11636.Hello World.cpp
+++ 11636.Hello World.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 int main () {
 
+  int a[20] = {0};
+  a[0] = 1;
+  a[1] = 2;
+
+  for (int i = 2; i < 20; i++) {
+    a[i] = a[i - 1] * 2;
+  }
+
+  int cas = 1;
+  int n;
+
+  while (cin >> n) {
+    for (int i = 0; i < n; i++) {
+      if (a[i] >= n) {
+        cout << "Cas " << cas << ": "<< i << endl;
+        cas++;
+        break;
+      }
+    }
+  }
+  
   
 return 0;
 }
