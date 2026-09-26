6d7867882b3c01441c57f69e415898a24ffc2328
SANIA SAEL <159134444+Sania268@users.noreply.github.com>
2026-09-26 23:29:24 +0800

    Add scoring logic for consecutive 'O' inputs
    Implement scoring system based on consecutive 'O' characters.

--- 1585.Score.cpp
+++ 1585.Score.cpp
@@ -1,6 +1,29 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 using namespace std;
 
 int main () {
-  return 0;
+    int T;
+    cin >> T;
+
+    while (T--) {
+
+        string s;
+        cin >> s;
+
+        int score = 0;
+        int consecutive = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+if (s[i] =='O') {
+    consecutive++;
+    score += consecutive;
+    }
+    else if (s[i] =='X') {
+        consecutive = 0;
+    }
+    }
+     cout << score << endl;
+}
+    return 0;
 }
